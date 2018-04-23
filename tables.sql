6c5f7adbd73870e75df9de175794c11c4fdb0440
Sarah <sarahhale95@gmail.com>
2018-04-23 13:03:28 +0200

    data added

--- tables.sql
+++ tables.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS billing (
 
 CREATE TABLE IF NOT EXISTS subscribers(
   su_id INT NOT NULL AUTO_INCREMENT,
-  su_socialnumber INT NOT NULL,
+  su_socialnumber varchar(10),
   su_firstname varchar(64),
   su_lastname varchar(64),
   su_address varchar(200),
@@ -40,3 +40,8 @@ INSERT INTO company(co_corporateNumber,co_name,co_telephone,co_address, co_postc
 (7493342385,'Cool Shit', '034 345 23 53', 'Skidspåret 4', '098 23', 'Kiruna');
 
 INSERT INTO billing(bi_address, bi_postcode,bi_city,co_id) VALUES ('Vägen 1','090 23','Umeå',1);
+
+INSERT INTO subscribers(su_socialnumber,su_firstname,su_lastname,su_address,su_postcode,su_city,su_subscribtion_number) VALUES
+('9503040332','Johan','Larsson','Huset 45', '341 34','Stockholm',34),
+('9104069881','Magda','Hirsch','Balkongen 5','908 23','Ystad',213),
+('6501125567','My','Andersson','Terassen 8', '092 32','Bornholm',9);
