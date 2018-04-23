f4892537e096f1b5877aefee18a5278b9ce73c44
Sarah <sarahhale95@gmail.com>
2018-04-23 11:29:52 +0200

    dependecies

---
+++ setup.js
@@ -0,0 +1,30 @@
+let mysql = require('mysql');
+var fs = require('fs');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "mydb"
+});
+
+let queries = fs.readFileSync('./tables.sql').toString().split(';');
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  setupDB(queries)
+
+});
+
+const setupDB = (queries) => {
+  for (let i = 0; i < queries.length - 1; i++) {
+    sql = queries[i] + ';'.trim();
+    console.log(sql);
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+    });
+
+  }
+}
